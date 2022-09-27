d9c3bf3922ce041e5353c549cfd6cdc263b8ff36
uhhitsnathan <nathans2003@icloud.com>
2022-09-26 23:05:31 -0700

    pseudocode

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 
 let gameArray = ['rock', 'paper', 'scissors'];
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
 function getComputerChoice(){
     let randomIndex = Math.floor(Math.random()*gameArray.length);
-    let randomChoice = gameArray[randomIndex];
-    return randomChoice;
+    var result = gameArray[randomIndex];
+    return result;
 }
+function getPlayerChoice(){
+    let playerInput = prompt('CHOOSE ROCK PAPER OR SCISSORS');
+    return playerInput;
+}
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return 'Its a tie!';
+      } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+          return 'You lose! Papper beats rock!';
+        } else {
+          return 'You win! Rock beats scissors!';
+        }
+      } else if (playerSelection === 'papper') {
+        if (computerSelection === "rock") {
+          return 'You win! Papper beats rock!';
+        } else {
+          return 'You loose! Scissors beat papper';
+        }
+      } else {
+        if (computerSelection === 'rock') {
+          return 'You loose!! Rock beat scissors!';
+        } else {
+          return 'You win!! Scissors beat papper!';
+        }
+      }
+    }
+
+
+console.log(playRound(playerSelection, computerSelection));
